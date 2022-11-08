00f0827602ff96a9807e4c9eab8348095ac30c83
Briana Cortés <113396622+bcortes6@users.noreply.github.com>
2022-11-08 11:46:27 -0600

    Add files via upload

---
+++ BasedeDatosSQLite/BasedeDatosSQLite/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace BasedeDatosSQLite
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.nombre = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(48, 127);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nombre";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(48, 174);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(18, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "ID";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(111, 127);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(137, 20);
+            this.txtNombre.TabIndex = 3;
+            // 
+            // txtID
+            // 
+            this.txtID.Location = new System.Drawing.Point(111, 174);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(137, 20);
+            this.txtID.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(51, 284);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(197, 26);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Insertar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(51, 325);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(197, 26);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Actualizar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(51, 369);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(197, 27);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Borrar";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.Control;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.nombre,
+            this.id});
+            this.dataGridView1.Location = new System.Drawing.Point(279, 115);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(511, 302);
+            this.dataGridView1.TabIndex = 9;
+            // 
+            // nombre
+            // 
+            this.nombre.HeaderText = "Nombre";
+            this.nombre.Name = "nombre";
+            // 
+            // id
+            // 
+            this.id.HeaderText = "Id";
+            this.id.Name = "id";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(257, 42);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(286, 31);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Base de Datos en SQLite";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(833, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtID);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nombre;
+        private System.Windows.Forms.DataGridViewTextBoxColumn id;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ BasedeDatosSQLite/BasedeDatosSQLite/Form1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SQLite;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BasedeDatosSQLite
+{
+    public partial class Form1 : Form
+    {
+        //agregamos la base de datos
+        string path = "data_table.db";
+        string cs = @"URI=file:" + Application.StartupPath + "\\data_table.db";
+        //la base de datos se crea en la carpeta DEBUG
+
+        SQLiteConnection conn;
+        SQLiteCommand cmd;
+        SQLiteDataReader dr;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            var conn = new SQLiteConnection(cs);
+            conn.Open();
+            var cmd = new SQLiteCommand(conn);
+            try
+            {
+                cmd.CommandText = "INSERT INTO text(name,id) VALUES(@name,@id)";
+
+                string NAME = txtNombre.Text;
+                string ID = txtID.Text;
+                cmd.Parameters.AddWithValue("@name", NAME);
+                cmd.Parameters.AddWithValue("@id", ID);
+
+                dataGridView1.ColumnCount = 2;
+                dataGridView1.Columns[0].Name = "Nombre";
+                dataGridView1.Columns[1].Name = "Id";
+                string[] row = new string[] { NAME, ID };
+                dataGridView1.Rows.Add(row);
+                cmd.ExecuteNonQuery();
+
+            }
+            catch(Exception)
+            {
+                Console.WriteLine("No se pudo insertar la imformación");
+                return;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var conn = new SQLiteConnection(cs);
+            conn.Open();
+            var cmd = new SQLiteCommand(conn);
+            try
+            {
+                cmd.CommandText = "UPDATE test set id=@Id where name = @Name";
+                cmd.Prepare();
+                cmd.Parameters.AddWithValue("@Name", txtNombre.Text);
+                cmd.Parameters.AddWithValue("@ID", txtID.Text);
+
+                cmd.ExecuteNonQuery();
+                dataGridView1.Rows.Clear();
+                data_show();
+
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("No se pudo actualizar la información");
+                return;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            var conn = new SQLiteConnection(cs);
+            conn.Open();
+            var cmd = new SQLiteCommand(conn);
+            try
+            {
+                cmd.CommandText = "DELATE FROM test WHERE name = @Name";
+                cmd.Prepare();
+                cmd.Parameters.AddWithValue("@Name", txtNombre.Text);
+
+                cmd.ExecuteNonQuery();
+                dataGridView1.Rows.Clear();
+                data_show();
+
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("No se pudo eliminar la información");
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Create_db();
+            data_show();
+        }
+
+        private void Create_db()
+        {
+            if(!System.IO.File.Exists(path))
+            {
+                SQLiteConnection.CreateFile(path);
+                using (var sqlite = new SQLiteConnection(@"Data Source="+path))
+                {
+                    sqlite.Open();
+                    string sql = "create table test(name varchar(20), id varchar(12))";
+                    SQLiteCommand command = new SQLiteCommand(sql,sqlite);
+                    command.ExecuteNonQuery();
+                }
+                   
+            }
+            else
+            {
+                Console.WriteLine("La base de datos no se puede crear");
+            }
+        }
+        private void data_show()
+        {
+            var conn = new SQLiteConnection(cs);
+            conn.Open();
+
+            string stm = "SELECT * FROM test";
+            var cmd = new SQLiteCommand(stm, conn);
+            dr = cmd.ExecuteReader();
+            while(dr.Read())
+            {
+                dataGridView1.Rows.Insert(0, dr.GetString(0), dr.GetString(1));
+            }
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
